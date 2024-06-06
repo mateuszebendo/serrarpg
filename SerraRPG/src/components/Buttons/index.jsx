59b743508188bef4a74a6e78c39cc376571237b4
ArturFoster <arturfosterdesouza@gmail.com>
2024-06-06 16:41:12 -0300

    feat: Base do Site Itens; style: Atualizar as cores do Button

--- SerraRPG/src/components/Buttons/index.jsx
+++ SerraRPG/src/components/Buttons/index.jsx
@@ -3,13 +3,12 @@ import { useState } from "react";
 import styles from "./styles.module.css";
 
 const Button = ({ title }) => {
-    const [estilo,setEstilo] = useState(true);
-
+    /*const [estilo,setEstilo] = useState(true);
     function trocarStyle(){
         setEstilo(!estilo);
-    }
+    }*/    
 
-    return <button className={estilo?styles.butt:styles.butt2} onClick={trocarStyle}>
+    return <button className={styles.butt}>
         {title}
     </button>
 }
